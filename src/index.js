cba9dfe0278cd0bfa814bb94fa6068e53e6e5888
Vladimir <krowen@inbox.ru>
2020-04-27 11:28:17 +0600

    Добавил опцию --format

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import program from 'commander';
 const getProgram = () => {
   program
     .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
+    .option('-f, --format [type]', 'output format');
 
   program.parse(process.argv);
 };
