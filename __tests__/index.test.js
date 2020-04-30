acd2cd718ad2b17830ce9e6643e4ec772d2556b4
Vladimir <krowen@inbox.ru>
2020-04-30 11:08:12 +0600

    balbalbal

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,5 +10,5 @@ test('gendiff', () => {
  - follow: false
  + verbose: true
 }`;
-  expect(genDiff('../before.json', '../after.json')).toEqual(result);
+  expect(genDiff('before.json', 'after.json')).toEqual(result);
 });
