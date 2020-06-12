59d834b17eebeeda14d34efd6b1c868a3160c8d4
Vladimir <krowen@inbox.ru>
2020-06-12 12:22:02 +0600

    Подключил все модули в точку входа

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import _ from 'lodash';
-import getParsedFiles from './parsers.js';
+import createDiff from './createDiff.js';
+import renderTree from './stylish.js';
 
 const genDiff = (pathToFile1, pathToFile2) => {
-  if (pathToFile1 === undefined || pathToFile2 === undefined) {
-    throw new Error('Files not found');
-  }
-  const [firstFile, secondFile] = getParsedFiles(pathToFile1, pathToFile2);
-  const firstFileKeys = Object.keys(firstFile);
-  const secondFileKeys = Object.keys(secondFile);
-  const allKeys = _.uniq([...firstFileKeys, ...secondFileKeys]);
-  const result = allKeys.reduce((acc, item) => {
-    const valueFirst = _.get(firstFile, item);
-    const valueSecond = _.get(secondFile, item);
-    if (_.has(firstFile, item) && _.has(secondFile, item)) {
-      return (valueFirst === valueSecond)
-        ? [...acc, [`   ${item}: ${valueFirst}`]]
-        : [...acc, [` + ${item}: ${valueSecond}`], [` - ${item}: ${valueFirst}`]];
-    }
-    return (_.has(firstFile, item) && !_.has(secondFile, item))
-      ? [...acc, [` - ${item}: ${valueFirst}`]] : [...acc, [` + ${item}: ${valueSecond}`]];
-  }, []);
-  return `{\n${result.join('\n')}\n}`;
+  const diff = createDiff(pathToFile1, pathToFile2);
+  const tree = renderTree(diff);
+  return tree;
 };
 
 export default genDiff;
