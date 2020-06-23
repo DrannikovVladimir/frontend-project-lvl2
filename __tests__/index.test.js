dde171214f202a4e2928ff6020182eb5c2c3e100
Vladimir <krowen@inbox.ru>
2020-06-23 15:49:31 +0600

    Переделал тесты. Изменил фикстуры

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,62 +2,22 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-test('gendiff', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const firstFile = getFixturePath('first.json') || getFixturePath('first.yml') || getFixturePath('first.ini');
-  const secondFile = getFixturePath('second.json') || getFixturePath('second.yml') || getFixturePath('second.ini');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+const getExpectedResult = (format) => readFile(`result_${format}.txt`);
+const formats = ['nested', 'plain', 'json'];
+const extentions = ['json', 'yml', 'ini'];
 
-  const tree =
-`{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: {
-            key: value
-        }
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-    }
-  + group3: {
-        fee: 100500
-    }
-}`;
+formats.forEach((format) => {
+  test.each(extentions)(
+    `genDiff extension: %s, format: ${format}`,
+    (extention) => {
+      const pathToFileFirst = getFixturePath(`first.${extention}`);
+      const pathToFileSecond = getFixturePath(`second.${extention}`);
+      const currentResult = genDiff(pathToFileFirst, pathToFileSecond, format);
+      const expectedResult = getExpectedResult(format);
 
-  const plain =
-`Property common.follow was added with value: false
-Property common.setting2 was deleted
-Property common.setting3 was changed from 'true' to '[complex value]'
-Property common.setting4 was added with value: blah blah
-Property common.setting5 was added with value: [complex value]
-Property common.setting6.ops was added with value: vops
-Property group1.baz was changed from 'bas' to 'bars'
-Property group1.nest was changed from '[complex value]' to 'str'
-Property group2 was deleted
-Property group3 was added with value: [complex value]`;
-
-  const fileJson = fs.readFileSync(getFixturePath('result.json'), 'utf-8').trim();
-
-  expect(genDiff(firstFile, secondFile, 'nested')).toEqual(tree);
-  expect(genDiff(firstFile, secondFile, 'plain')).toEqual(plain);
-  expect(genDiff(firstFile, secondFile, 'json')).toEqual(fileJson);
+      expect(currentResult).toBe(expectedResult);
+    },
+  );
 });
